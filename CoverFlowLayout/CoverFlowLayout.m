6cfc9627ccbe99020f1e1302d5b8b837cacae236
Alp Eren Can <alperenc@gmail.com>
2015-09-10 13:43:23 -0700

    add a custom flow layout and hook it up in storyboard

---
+++ CoverFlowLayout/CoverFlowLayout.m
@@ -0,0 +1,13 @@
+//
+//  CoverFlowLayout.m
+//  CoverFlowLayout
+//
+//  Created by Alp Eren Can on 10/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import "CoverFlowLayout.h"
+
+@implementation CoverFlowLayout
+
+@end
