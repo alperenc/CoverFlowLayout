2b4d2a1963688e374b130dbc9192f9c69d0cf5aa
Alp Eren Can <alperenc@gmail.com>
2015-09-10 13:33:46 -0700

    Initial Commit

---
+++ CoverFlowLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoverFlowLayout
+//
+//  Created by Alp Eren Can on 10/09/15.
+//  Copyright © 2015 Alp Eren Can. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
